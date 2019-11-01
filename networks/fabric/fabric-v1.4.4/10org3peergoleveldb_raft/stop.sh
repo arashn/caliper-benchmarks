15faf99c68a31ccd8dd2c3800bf44b612aca8078
Arash Nabili <arash@levyx.com>
2019-11-01 16:12:13 -0700

    Updated stop script

--- networks/fabric/fabric-v1.4.4/10org3peergoleveldb_raft/stop.sh
+++ networks/fabric/fabric-v1.4.4/10org3peergoleveldb_raft/stop.sh
@@ -7,8 +7,11 @@ docker stack rm fabric-orderer
 echo "docker stack rm fabric-ca"
 docker stack rm fabric-ca
 
-echo "(test -z \"$(docker images dev* -q)\") || docker rmi $(docker images dev* -q)"
-(test -z \"$(docker images dev* -q)\") || docker rmi $(docker images dev* -q)
+for node in "$@"
+do
+    echo ssh $node '(test -z "$(docker ps -aq)") || docker stop $(docker ps -aq); (test -z "$(docker ps -aq)") || docker rm $(docker ps -aq); (test -z "$(docker images dev* -q)") || docker rmi $(docker images dev* -q)'
+    ssh $node '(test -z "$(docker ps -aq)") || docker stop $(docker ps -aq); (test -z "$(docker ps -aq)") || docker rm $(docker ps -aq); (test -z "$(docker images dev* -q)") || docker rmi $(docker images dev* -q)'
+done
 
 echo "rm -rf /tmp/hfc-*"
 rm -rf /tmp/hfc-*
